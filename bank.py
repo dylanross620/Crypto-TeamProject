9b5b972cc5a040199fc479e3e559b206dbd66bda
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2020-08-06 14:20:57 -0400

    more bank stuff

--- bank.py
+++ bank.py
@@ -1,11 +1,17 @@
 import json
 
 class Bank:
-    def __init__(self):
+    def __init__(self, atmpreflist = []):
         self.usertopass = json.loads(open("local_storage/usertohashpass.txt", "r").read()) #returns dict structured (user:hashpass)
         self.usertomoney = json.loads(open("local_storage/usertomoney.txt", "r").read()) #returns dict structured (user:plaintext_money)
         self.methods = ['rsa', 'elgamal'] #in order of preference
-
+        atmpreflist = [x.lower() for x in atmpreflist]
+        self.common = list(set(self.methods) & set(atmpreflist))
+        if len(self.common) == 0:
+            print("no common methods between atm/bank")
+        else:
+            self.common = self.common[0]
+        
     def addhashedpassword(username: str, password: str):
         self.usertopass[username] = password
         open("local_storage/usertohashpass.txt", "w+").write(json.dumps(self.usertopass))
@@ -15,10 +21,11 @@ class Bank:
         open("local_storage/usertomoney.txt", "w+").write(json.dumps(self.usertomoney))
 
 if __name__ == "__main__":
-    testbank = Bank()
+    testbank = Bank(["rsa"])
     print(testbank.usertopass)
     print(testbank.usertomoney)
     for k in testbank.usertomoney.keys():
         print("USER TO MONEY --> %s: %s" % (k, testbank.usertomoney[k]))
     for k in testbank.usertopass.keys():
         print("USER TO PASS --> %s: %s" % (k, testbank.usertopass[k]))
+    print(testbank.common)
