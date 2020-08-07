2c3b7651e77362891bd5855cedc5d9ab0ed1b162
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2020-08-07 15:19:06 -0400

    some handshake stuff

--- atm.py
+++ atm.py
@@ -1,10 +1,38 @@
 import json
-
+import secrets
+from PublicKey import rsa
+from PublicKey import elgamal
 class ATM:
-    def __init__(self):
+    def __init__(self, username, password, preflist = []):
+        self.user = username
+        self.pw  = password
+        # self.client_random = secrets.token_bytes(4096)
+        # self.premaster = secrets.token_bytes(4096)
+        if len(preflist) == 0:
+            raise Exception("need to have preferences as the user to compare to server...")
+        self.prefs = preflist
+        self.scheme = None
+        self.keypairs = None
+        # self.bankrandom = None
+    def withdraw_money(self):
         pass
 
+    def deposit_money(self):
+        pass
+
+    def key_setup(self):
+        if self.scheme == None:
+            raise Exception("need to assign common scheme in atm!")
+        if self.scheme == "rsa":
+            self.keypairs = rsa.load_keys("local_storage/atm-rsa.txt", 4096)
+        else:
+            self.keypairs = elgamal.load_keys("local_storage/atm-elgamal.txt",4096)
 
 
 if __name__ == "__main__":
-    atmtest = ATM()
+    # atmtest = ATM()
+    g = 6497955158
+    m = 1
+    n = 126869
+    r = 35145
+    print((g * (r**n) % (n**2)))
--- bank.py
+++ bank.py
@@ -1,32 +1,58 @@
 import json
 from PublicKey import rsa
+from PublicKey import elgamal
+import hash
+from atm import ATM
+import secrets
+
 class Bank:
-    def __init__(self, atmpreflist = []):
+    def __init__(self):
         self.usertopass = json.loads(open("local_storage/usertohashpass.txt", "r").read()) #returns dict structured (user:hashpass)
         self.usertomoney = json.loads(open("local_storage/usertomoney.txt", "r").read()) #returns dict structured (user:plaintext_money)
         self.methods = ['rsa', 'elgamal'] #in order of preference
-        atmpreflist = [x.lower() for x in atmpreflist]
-        self.common = list(set(self.methods) & set(atmpreflist))
-        if len(self.common) == 0:
-            raise Exception("no common methods between atm/bank")
-        else:
-            self.common = self.common[0]
-        self.read = rsa.load_keys("bank-" + self.common + ".txt", 128)
-    def addhashedpassword(username: str, password: str):
-        self.usertopass[username] = password
+        print("Public key methods in use by bank --> ", self.methods)
+        # self.read = rsa.load_keys("local_storage/bank-" + self.common + ".txt", 128)
+        # self.server_random = secrets.token_bytes(4096)
+        # self.atmrandom = None
+        self.scheme = None
+        self.keypairs = None
+        # print(self.server_random)
+    def addhashedpassword(self, username: str, password: str) :
+        self.usertopass[username] = hash.sha256(password)
         open("local_storage/usertohashpass.txt", "w+").write(json.dumps(self.usertopass))
 
-    def addusertomoney(username: str, amount: str): #adds info to runtime dict and dumps to file
+    def addusertomoney(self, username: str, amount: str): #adds info to runtime dict and dumps to file
         self.usertomoney[username] = amount
         open("local_storage/usertomoney.txt", "w+").write(json.dumps(self.usertomoney))
 
+    def starthandshake(self, atminstance): #encrypt username with atm public key, and send it back (deny connection if username doesnt exist)
+        print(f"ATM user '{atminstance.user}' has initiated handshake, hello to BANK server!")
+        #we now take in the client random byte string, client supported encryptions, and 
+        # self.atmrandom = atminstance.client_random
+        # print(f"Handshake info --> client random recieved in plaintext as {clientrand}")
+        print(f"Handshake info --> client supported schemes {atminstance.prefs}")
+        print(f"Handshane info --> starting server hello...")
+        atmpreflist = [x.lower() for x in atminstance.prefs]
+        common = list(set(self.methods) & set(atmpreflist))
+        if len(common) == 0:
+            raise Exception("no common methods between atm/bank")
+        else:
+            self.scheme = common[0]
+        atminstance.scheme = self.scheme
+        print(f"Handshake info --> common encryption scheme set to use {self.scheme}")
+        if self.scheme == "rsa":
+            self.keypairs = rsa.load_keys("local_storage/bank-rsa.txt", 4096)
+        else:
+            self.keypairs = elgamal.load_keys("local_storage/bank-elgamal.txt",4096)
+        atminstance.key_setup()
+        # print(f"Handshake info --> server random sent in plaintext as {self.server_random}")
+        # atminstance.bankrandom = self.server_random
+
+
 if __name__ == "__main__":
-    testbank = Bank(["rsa"])
+    testbank = Bank()
+    testatm = ATM("testuser","testpass", ['rsa'])
+    testbank.starthandshake(testatm)
     print(testbank.usertopass)
     print(testbank.usertomoney)
-    for k in testbank.usertomoney.keys():
-        print("USER TO MONEY --> %s: %s" % (k, testbank.usertomoney[k]))
-    for k in testbank.usertopass.keys():
-        print("USER TO PASS --> %s: %s" % (k, testbank.usertopass[k]))
-    print(testbank.common)
-    print(self.read)
+    # print(testbank.keypairs)
