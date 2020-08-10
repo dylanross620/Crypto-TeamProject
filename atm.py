7b80cec2890257d271d1c943382eb2eadd216bf6
Lreardon1 <leithreardon@gmail.com>
2020-08-10 08:10:02 -0400

    Merge branch 'master' of https://github.com/dylanross620/Crypto-TeamProject

--- atm.py
+++ atm.py
@@ -19,7 +19,11 @@ class ATM:
         self.pubkey = None
         self.privkey = None
         self.bankpubkey = None
-        self.counter = 1;
+        self.p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF
+        self.dhprivateaes = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.dhprivatemac = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.clientrandom = secrets.token_bytes(32)
+        self.counter = 1
         self.s = socket.socket()
         self.s.connect(('127.0.0.1', 5432))
 
@@ -30,6 +34,7 @@ class ATM:
 
     def post_handshake(self): #takes in user input to interact with bank indefinitely
         print("ATM")
+        print("Example login(atm will try to verify with your initalized user/pass): LOGIN")
         print("Example withdraw: 'withdraw [positive_int]'")
         print("Example deposit: 'deposit [positive_int]'")
         print("Example check: 'check balance'")
@@ -62,31 +67,32 @@ class ATM:
             inp = inp.strip()
             if inp == 'q':
                 break
-            inp = inp.split(' ')
-            sendstr = self.user + '-' + self.pw + '-'
-            if len(inp) != 2:
-                print("not a valid operation supported by bank")
-                continue
-            if inp[0].lower() == 'withdraw':
-                sendstr += inp[0].lower()
-            elif inp[0].lower() == 'deposit':
-                sendstr += inp[0].lower()
-            elif inp[0].lower() == 'check':
-                sendstr += inp[0].lower()
+            sendstr = self.user + '-'
+            if inp.lower() == 'login':
+                sendstr += inp.lower() + '-' + self.pw
             else:
-                print("not a valid operation supported by bank")
-                continue
-            if inp[1].isnumeric() and int(inp[1]) > 0:
-                sendstr += '-' + inp[1]
-            elif inp[1].lower() == 'balance':
-                sendstr += '-' + inp[1]
-            else:
-                print("invalid money amount")
-                continue
-
+                inp = inp.split(' ')
+                if len(inp) != 2:
+                    print("not a valid operation supported by bank")
+                    continue
+                if inp[0].lower() == 'withdraw':
+                    sendstr += inp[0].lower()
+                elif inp[0].lower() == 'deposit':
+                    sendstr += inp[0].lower()
+                elif inp[0].lower() == 'check':
+                    sendstr += inp[0].lower()
+                else:
+                    print("not a valid operation supported by bank")
+                    continue
+                if inp[1].isnumeric() and int(inp[1]) > 0:
+                    sendstr += '-' + inp[1]
+                elif inp[1].lower() == 'balance':
+                    sendstr += '-' + inp[1]
+                else:
+                    print("invalid money amount")
+                    continue
             sendstr = str(self.counter) + '-' + sendstr
-            #in bank, verify the hash including all dashes except the one right before the sha
-            sendstr = aes.encrypt(sendstr + "-" + hash.sha1(sendstr),self.aeskey)
+            sendstr = aes.encrypt(sendstr + "-" + hash.hmac(sendstr,self.mackey),self.aeskey)
             self.s.send(sendstr.encode('utf-8'))
 
             bankret = self.s.recv(99999).decode('utf-8')#parse this out
@@ -101,11 +107,8 @@ class ATM:
             bankret.remove(chkhash)
             againsthash = '-'.join(bankret)
             bankret = bankret[1:]
-            if hash.sha1(againsthash) != chkhash:
+            if hash.hmac(againsthash,self.mackey) != chkhash:
                 print("bank return msg integrity compromised")
-                continue
-            if bankret[0] != self.user:
-                print("bank user return value tampered with")
                 continue
             print(f"bank responded with '{bankret[2]}' to the request, money in account: {bankret[1]}") 
         self.s.close()
@@ -124,38 +127,36 @@ class ATM:
             self.bankpubkey = bkeypairs[0] # see above
         self.pubkey = keypairs[0]
         self.privkey = keypairs[1]
-        print("Handshake info --> sending MAC key to bank...")
-        macsender = self.mackey + '-' + hash.sha1(self.mackey)
+        print("Handshake info --> sending client random")
+        self.s.send(str(self.clientrandom).encode('utf-8'))
+        clirandplain = self.s.recv(99999).decode('utf-8')
+        self.s.send("recieved plaintext signature".encode('utf-8'))
+        clirandsign = self.s.recv(4096).decode('utf-8')
         if self.scheme == 'rsa':
-            macsender = rsa.encrypt(macsender,self.bankpubkey)
+            clirandsign = rsa.verify_signature(int(clirandsign),clirandplain,self.bankpubkey)
         else:
-            macsender = elgamal.encrypt(macsender,self.bankpubkey)
-        self.s.send(str(macsender).encode('utf-8'))
-        print(f"Handshake info --> bank returned {self.s.recv(4096).decode('utf-8')}")
-        print("Handshake info --> sending AES key...")
-        if self.scheme == 'rsa':
-            aestmp = self.aeskey + '-' + hash.sha1(self.aeskey)
-            aestmp = rsa.encrypt(aestmp, self.bankpubkey)
-            self.s.send(str(aestmp).encode('utf-8'))
-            print(f"Handshake info --> AES key sent, bank replied {self.s.recv(1024).decode('utf-8')}")
-        else:
-            aestmp1of2 = self.aeskey[:len(self.aeskey)//2]
-            aestmp2of2 = self.aeskey[len(self.aeskey)//2:]
-            aestmp1of2 = aestmp1of2 + '-' + hash.sha1(aestmp1of2)
-            aestmp2of2 = aestmp2of2 + '-' + hash.sha1(aestmp2of2)
-            aestmp1of2 = elgamal.encrypt(aestmp1of2, self.bankpubkey)
-            aestmp2of2 = elgamal.encrypt(aestmp2of2, self.bankpubkey)
-            self.s.send(str(aestmp1of2).encode('utf-8'))
-            print(f"Handshake info --> AES block 1/2 sent, bank replied {self.s.recv(1024).decode('utf-8')}") #bank replied good block
-            self.s.send(str(aestmp2of2).encode('utf-8'))
-            print(f"Handshake info --> AES block 2/2 sent, bank replied {self.s.recv(1024).decode('utf-8')}") #bank replied good block
+            clirandsign = clirandsign.strip("(").strip(")").split(",")
+            clirandsign = [x.strip() for x in clirandsign] #take away tuple space or wierd stuff
+            clirandsign = (int(clirandsign[0]), int(clirandsign[1]))
+            clirandsign = elgamal.verify_signature(clirandsign,clirandplain,self.bankpubkey)
 
-        print("Handshake info --> sending over atm pubkey...")
-        pubkeysender = aes.encrypt((str(self.pubkey) + '-' + hash.hmac(str(self.pubkey),self.mackey)),self.aeskey)
-        self.s.send(pubkeysender.encode('utf-8'))
-        pubkeyret = self.s.recv(4096).decode('utf-8')
-        print(f"Handshake info --> bank returned {pubkeyret}")
-        print("Handshake info --> ATM ready to go!")
+        if clirandsign:
+            self.s.send("signature verify success".encode('utf-8'))
+        else:
+            self.s.send("signature verify failed".encode('utf-8'))
+            self.s.close()
+            raise Exception("signature verify failed")
+        self.s.recv(4096) #formatting
+        print("Handshake info --> bank signature verified, DH parameters recieved")
+        self.s.send((str(pow(2, self.dhprivateaes, self.p)) + '-' + str(pow(2, self.dhprivatemac, self.p))).encode('utf-8'))
+        clirandplain = clirandplain.split('-')
+        self.aeskey = pow(int(clirandplain[-2]),self.dhprivateaes,self.p) % pow(2,256)
+        self.mackey = pow(int(clirandplain[-1]),self.dhprivatemac,self.p) % pow(2,256)
+        self.aeskey = str(hex(self.aeskey))[2:]
+        self.mackey = str(hex(self.mackey))[2:]
+        print("Handshake info --> atm calculated aes/mac keys from DH exchange")
+        self.s.send((aes.encrypt("finished",self.aeskey)).encode('utf-8'))
+        print(f"Handshake info --> ATM ready to go, bank replied {aes.decrypt(self.s.recv(1024).decode('utf-8'),self.aeskey)}")
         self.post_handshake()
 
 if __name__ == "__main__":
--- bank.py
+++ bank.py
@@ -2,7 +2,7 @@ import json
 import hash
 import socket
 import select
-import ast
+import secrets
 from PublicKey import rsa
 from PublicKey import elgamal
 from PrivateKey import aes
@@ -20,18 +20,21 @@ class Bank:
         self.aeskey = None
         self.mackey = None
         self.counter = 0
-
+        self.p= 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF
+        self.dhprivateaes = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.dhprivatemac = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.serverrandom = secrets.token_bytes(32)
         self.s = socket.socket()
         self.s.bind(('127.0.0.1',5432))
         self.s.listen(5)
         self.client = None
         self.clientaddr = None
 
-    def addhashedpassword(self, username: str, password: str) :
+    def addhashedpassword(self, username: str, password: str): #for testing use only, you shouldn't be able to create bank account at atm
         self.usertopass[username] = hash.sha1(password)
         open("local_storage/usertohashpass.txt", "w+").write(json.dumps(self.usertopass))
 
-    def addusertomoney(self, username: str, amount: str): #adds info to runtime dict and dumps to file
+    def addusertomoney(self, username: str, amount: str): #for testing use only
         self.usertomoney[username] = amount
         open("local_storage/usertomoney.txt", "w+").write(json.dumps(self.usertomoney))
 
@@ -48,28 +51,30 @@ class Bank:
         if int(self.usertomoney[usr]) - amt < 0:
             sendback += self.usertomoney[usr] + '-' + "cannot overdraw this account"
             sendback = str(self.counter) + '-' + sendback
-            sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+            sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
             self.client.send(sendback.encode('utf-8'))
         else:
             self.usertomoney[usr] = str(int(self.usertomoney[usr]) - amt)
+            open("local_storage/usertomoney.txt", "w+").write(json.dumps(self.usertomoney))
             sendback += self.usertomoney[usr] + '-' + "withdraw successful"
             sendback = str(self.counter) + '-' + sendback
-            sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+            sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
             self.client.send(sendback.encode('utf-8'))
 
     def deposit(self,usr, amt):
             sendback = usr + "-"
             self.usertomoney[usr] = str(int(self.usertomoney[usr]) + amt)
+            open("local_storage/usertomoney.txt", "w+").write(json.dumps(self.usertomoney))
             sendback += self.usertomoney[usr] + '-' + "deposit successful"
             sendback = str(self.counter) + '-' + sendback
-            sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+            sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
             self.client.send(sendback.encode('utf-8'))
 
     def check(self,usr):
             sendback = usr + "-"
             sendback += self.usertomoney[usr] + '-' + "check successful"
             sendback = str(self.counter) + '-' + sendback
-            sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+            sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
             self.client.send(sendback.encode('utf-8'))
             
     def post_handshake(self):
@@ -97,6 +102,8 @@ class Bank:
         sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
         self.client.send(sendback.encode('utf-8'))
 
+        loggedin = False
+        loginname = ""
         while True:
             cmd = self.client.recv(4096).decode('utf-8')
             if len(cmd) == 0:
@@ -108,36 +115,50 @@ class Bank:
             cmd.remove(chkhash)
             againsthash = '-'.join(cmd)
             cmd = cmd[1:]
-            if hash.sha1(againsthash) != chkhash:
+            if hash.hmac(againsthash,self.mackey) != chkhash:
                 sendback = "notverifieduser-0-msg integrity compromised"
                 sendback = str(self.counter) + '-' + sendback
-                sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+                sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
                 self.client.send(sendback.encode('utf-8'))
                 continue
             if cmd[0] not in list(self.usertopass.keys()):
-                sendback = "notverifieduser-0-username not known in bank"
+                sendback = "notverifieduser-0-username not known in bank(tampered name error)"
                 sendback = str(self.counter) + '-' + sendback
-                sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+                sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
                 self.client.send(sendback.encode('utf-8'))
                 continue
-            if cmd[1] != self.usertopass[cmd[0]]:
-                sendback = cmd[0] + "-"
-                sendback += self.usertomoney[usr] + '-' + "password not matching in bank"
-                sendback = str(self.counter) + '-' + sendback
-                sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
-                self.client.send(sendback.encode('utf-8'))
-                continue
-            if cmd[2] == 'withdraw':
-                self.withdraw(cmd[0],int(cmd[3]))
-            elif cmd[2] == 'deposit':
-                self.deposit(cmd[0],int(cmd[3]))
-            elif cmd[2] == 'check':
+            if cmd[1] == 'withdraw' and loggedin:
+                self.withdraw(cmd[0],int(cmd[2]))
+            elif cmd[1] == 'deposit' and loggedin:
+                self.deposit(cmd[0],int(cmd[2]))
+            elif cmd[1] == 'check' and loggedin:
                 self.check(cmd[0])
-            else:
-                sendback = cmd[0] + "-"
-                sendback += self.usertomoney[usr] + '-' + "invalid command"
+            elif cmd[1] == 'login':
+                if cmd[0] not in list(self.usertopass.keys()):
+                    sendback = "notverifieduser-0-username not known in bank"
+                    sendback = str(self.counter) + '-' + sendback
+                    sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
+                    self.client.send(sendback.encode('utf-8'))
+                if cmd[2] != self.usertopass[cmd[0]]:
+                    sendback = cmd[0] + "-0-password not matching in bank"
+                    sendback = str(self.counter) + '-' + sendback
+                    sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
+                    self.client.send(sendback.encode('utf-8'))
+                loggedin = True
+                loginname = cmd[0]
+                sendback = loginname + "-"
+                sendback += self.usertomoney[loginname] + '-' + "login successful"
                 sendback = str(self.counter) + '-' + sendback
-                sendback = aes.encrypt(sendback + '-' + hash.sha1(sendback),self.aeskey)
+                sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
+                self.client.send(sendback.encode('utf-8'))
+            else:
+                if loggedin:
+                    sendback = loginname + "-"
+                    sendback += self.usertomoney[loginname] + '-' + "invalid command"
+                else:
+                    sendback = "not_logged_in-0-LOGIN command not sent"
+                sendback = str(self.counter) + '-' + sendback
+                sendback = aes.encrypt(sendback + '-' + hash.hmac(sendback,self.mackey),self.aeskey)
                 self.client.send(sendback.encode('utf-8'))
             
         self.s.close()
@@ -149,12 +170,11 @@ class Bank:
         print(clienthello)
         clientname = repr(clienthello[0]).strip("'")
         atmprefs = json.loads(clienthello[1])
-        # if clientname not in list(self.usertopass.keys()):
-        #     raise Exception("Supplied atm username not in bank records")
         print(f"ATM user " + clientname + " has initiated handshake, hello to BANK server!")
         atmprefs = [x.lower() for x in atmprefs]
         common = list(set(self.methods) & set(atmprefs))
         if len(common) == 0:
+            self.s.close()
             raise Exception("no common methods between atm/bank")
         else:
             self.scheme = common[0]
@@ -167,87 +187,29 @@ class Bank:
         self.pubkey = keypairs[0]
         self.privkey = keypairs[1]
         self.client.send(self.scheme.encode('utf-8'))
-        #need to recieve mac key via asymmetric encryption
-        print(f"Handshake info --> acquiring MAC key...")
-        mactmp = self.client.recv(99999).decode('utf-8')
+        print("Handshake info --> recieving client random")
+        clirand = self.client.recv(4096).decode('utf-8')
+        print("Handshake info --> signing client random, server random, and DH parameters")
+        clisign = str(clirand) + '-' + str(self.serverrandom) + '-' + str(pow(2, self.dhprivateaes, self.p)) + '-' + str(pow(2, self.dhprivatemac, self.p))
+        clie = None
         if self.scheme == 'rsa':
-            mactmp = rsa.decrypt(int(mactmp),self.privkey)
+            clie = rsa.sign(clisign,self.privkey)
         else:
-            mactmp = mactmp.strip("(").strip(")").split(",")
-            mactmp = [x.strip() for x in mactmp] #take away tuple space or wierd stuff
-            mactmp = (int(mactmp[0]), int(mactmp[1]))
-            mactmp = elgamal.decrypt(mactmp,self.privkey)
-        mactmp = mactmp.split('-')
-        if hash.sha1(mactmp[0]) == mactmp[1]:
-            self.client.send("good MAC key recieved".encode('utf-8'))
-            self.mackey= mactmp[0]
-        else:
-            self.client.send("error in MAC key".encode('utf-8'))
-            raise Exception("error in mac key")
-
-        print("Handshake info --> MAC key acquired")
-        print("Handshake info --> starting AES shared key transfer")
-        if self.scheme == 'rsa':
-            tmpaes = self.client.recv(4096).decode('utf-8')
-            tmpaes = rsa.decrypt(int(tmpaes), self.privkey)
-            tmpaes = tmpaes.split('-')
-            if hash.sha1(tmpaes[0]) == tmpaes[1]:
-                print("Handshake info --> AES key recieved")
-                self.client.send("good AES key".encode('utf-8'))
-                self.aeskey = tmpaes[0]
-            else:
-                self.client.send("AES key tampered with".encode('utf-8'))
-                raise Exception("AES key tampered with")
-        else:
-            self.aeskey = ""
-            tmpaes1of2 = self.client.recv(4096).decode('utf-8').strip("(").strip(")").split(",")
-            tmpaes1of2 = [x.strip() for x in tmpaes1of2]
-            tmpaes1of2 = (int(tmpaes1of2[0]), int(tmpaes1of2[1])) 
-            tmpaes1of2 = elgamal.decrypt(tmpaes1of2, self.privkey)
-            tmpaes1of2 = tmpaes1of2.split('-')
-            if hash.sha1(tmpaes1of2[0]) == tmpaes1of2[1]:
-                print("Handshake info --> AES key block 1/2 recieved")
-                self.client.send("good block".encode('utf-8'))
-                self.aeskey += tmpaes1of2[0]
-            else:
-                self.client.send("bad block".encode('utf-8'))
-                raise Exception("AES key tampered with")
-
-            tmpaes1of2 = self.client.recv(4096).decode('utf-8').strip("(").strip(")").split(",")
-            tmpaes1of2 = [x.strip() for x in tmpaes1of2]
-            tmpaes1of2 = (int(tmpaes1of2[0]), int(tmpaes1of2[1])) 
-            tmpaes1of2 = elgamal.decrypt(tmpaes1of2, self.privkey)
-            tmpaes1of2 = tmpaes1of2.split('-')
-            if hash.sha1(tmpaes1of2[0]) == tmpaes1of2[1]:
-                print("Handshake info --> AES key block 2/2 recieved")
-                self.client.send("good block".encode('utf-8'))
-                self.aeskey += tmpaes1of2[0]
-            else:
-                self.client.send("bad block".encode('utf-8'))
-                raise Exception("AES key tampered with")
-
-        ##----now for the atmpubkey
-        print("Handshake info --> recieving atm pubkey...")
-        atmpubtmp = self.client.recv(99999).decode('utf-8')
-        atmpubtmp = aes.decrypt(atmpubtmp,self.aeskey)
-        atmpubtmp = atmpubtmp.split('-')
-        if hash.hmac(atmpubtmp[0],self.mackey) == atmpubtmp[1]:
-            self.client.send("good atmkey recieved".encode('utf-8'))
-        else:
-            self.client.send("bad atmkey".encode('utf-8'))
-            raise Exception("bad atmkey")
-        
-        if self.scheme == 'rsa':
-            atmpubtmp = atmpubtmp[0].strip("(").strip(")").split(",")
-            atmpubtmp = [x.strip() for x in atmpubtmp] #take away tuple space or wierd stuff
-            self.atmpubkey = (int(atmpubtmp[0]), int(atmpubtmp[1]))
-        else:
-            atmpubtmp = atmpubtmp[0].strip("(").strip(")").split(",")
-            atmpubtmp = [x.strip() for x in atmpubtmp] #take away tuple space or wierd stuff
-            self.atmpubkey = (int(atmpubtmp[0]), int(atmpubtmp[1]), int(atmpubtmp[2]))
-        
-        print("Handshake info --> atm pubkey successully recieved")
-        print("Handshake info --> Bank ready to go!")
+            clie = elgamal.sign(clisign,self.privkey, self.pubkey)
+        self.client.send(str(clisign).encode('utf-8'))
+        print(f"Handshake info --> client says {self.client.recv(4096).decode('utf-8')}")
+        self.client.send(str(clie).encode('utf-8'))
+        print(f"Handshake info --> client says {self.client.recv(4096).decode('utf-8')}")
+        self.client.send("breaker".encode('utf-8'))#formatting
+        cliplain = self.client.recv(99999).decode('utf-8')
+        cliplain = cliplain.split('-')
+        self.aeskey = pow(int(cliplain[0]),self.dhprivateaes,self.p) % pow(2,256)
+        self.mackey = pow(int(cliplain[1]),self.dhprivatemac,self.p) % pow(2,256)
+        self.aeskey = str(hex(self.aeskey))[2:]
+        self.mackey = str(hex(self.mackey))[2:]
+        print("Handshake info --> bank calculated aes/mac keys from DH exchange")
+        print(f"Handshake info --> Bank ready to go, atm replied {aes.decrypt(self.client.recv(1024).decode('utf-8'),self.aeskey)}")
+        self.client.send((aes.encrypt("finished",self.aeskey)).encode('utf-8'))
         self.post_handshake()
 
 if __name__ == "__main__":
