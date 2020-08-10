a2d3c61b49f840781c2e78d2b4d15be056770d41
Lreardon1 <leithreardon@gmail.com>
2020-08-10 13:54:31 -0400

    Moved random generation to handshake instead of init

--- atm.py
+++ atm.py
@@ -10,8 +10,8 @@ class ATM:
     def __init__(self, username, password, preflist = []):
         self.user = username
         self.pw  = hash.sha1(password)
-        self.aeskey = aes.generate_key()
-        self.mackey = hash.generate_mac_key()
+        self.aeskey = None
+        self.mackey = None
         if len(preflist) == 0:
             raise Exception("need to have preferences as the user to compare to server...")
         self.prefs = preflist
@@ -20,9 +20,9 @@ class ATM:
         self.privkey = None
         self.bankpubkey = None
         self.p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF
-        self.dhprivateaes = secrets.randbelow(((self.p - 1) // 2)+1)
-        self.dhprivatemac = secrets.randbelow(((self.p - 1) // 2)+1)
-        self.clientrandom = secrets.token_bytes(32)
+        self.dhprivateaes = None
+        self.dhprivatemac = None
+        self.clientrandom = None
         self.counter = 1
         self.id_num = 0 # each atm would have it's own id number
         self.s = socket.socket()
@@ -127,6 +127,7 @@ class ATM:
         self.pubkey = keypairs[0]
         self.privkey = keypairs[1]
         print("Handshake info --> sending client random")
+        self.clientrandom = secrets.token_bytes(32)
         self.s.send(str(self.clientrandom).encode('utf-8'))
         clirandplain = self.s.recv(99999).decode('utf-8')
         self.s.send("recieved plaintext signature".encode('utf-8'))
@@ -147,8 +148,12 @@ class ATM:
             raise Exception("signature verify failed")
         self.s.recv(4096) #formatting
         print("Handshake info --> bank signature verified, DH parameters recieved")
+        self.dhprivateaes = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.dhprivatemac = secrets.randbelow(((self.p - 1) // 2)+1)
         self.s.send((str(pow(2, self.dhprivateaes, self.p)) + '-' + str(pow(2, self.dhprivatemac, self.p))).encode('utf-8'))
         clirandplain = clirandplain.split('-')
+        # self.aeskey = aes.generate_key()
+        # self.mackey = hash.generate_mac_key()
         self.aeskey = pow(int(clirandplain[-2]),self.dhprivateaes,self.p) % pow(2,256)
         self.mackey = pow(int(clirandplain[-1]),self.dhprivatemac,self.p) % pow(2,256)
         self.aeskey = str(hex(self.aeskey))[2:]
--- bank.py
+++ bank.py
@@ -21,9 +21,9 @@ class Bank:
         self.mackey = None
         self.counter = 0
         self.p= 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF
-        self.dhprivateaes = secrets.randbelow(((self.p - 1) // 2)+1)
-        self.dhprivatemac = secrets.randbelow(((self.p - 1) // 2)+1)
-        self.serverrandom = secrets.token_bytes(32)
+        self.dhprivateaes = None
+        self.dhprivatemac = None
+        self.serverrandom = None
         self.s = socket.socket()
         self.s.bind(('127.0.0.1',5432))
         self.s.listen(5)
@@ -190,6 +190,9 @@ class Bank:
         print("Handshake info --> recieving client random")
         clirand = self.client.recv(4096).decode('utf-8')
         print("Handshake info --> signing client random, server random, and DH parameters")
+        self.dhprivateaes = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.dhprivatemac = secrets.randbelow(((self.p - 1) // 2)+1)
+        self.serverrandom = secrets.token_bytes(32)
         clisign = str(clirand) + '-' + str(self.serverrandom) + '-' + str(pow(2, self.dhprivateaes, self.p)) + '-' + str(pow(2, self.dhprivatemac, self.p))
         clie = None
         if self.scheme == 'rsa':
